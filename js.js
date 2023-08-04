4837f5d47702150aa04d474fdf2806f7385578d4
Oskaratam <oskaratamanenko2@gmail.com>
2023-08-04 23:04:57 +0300

    created an error,added responsive background

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
 const apiKey = '68e1a70385c4447ec9536bfd8ee37f28';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=';
 const input = document.getElementById('placeInput'),
-      searchBtn = document.getElementById('searchIcon');
+      searchBtn = document.getElementById('searchIcon'),
+      clouds = document.getElementById('clouds');
 
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404){
+        document.querySelector('.error').style.display = 'block';
+    }else {
     let data = await response.json();
 
     document.getElementById('placeName').innerHTML = data.name;
@@ -14,8 +19,10 @@ async function checkWeather(city){
     document.getElementById('humidity').innerHTML = `Humidity: ${data.main.humidity}%`;
     document.getElementById('feelsLike').innerHTML = `Feels like: ${Math.round(data.main.feels_like)}°C`;
     console.log(data);
-
-
+    const changeBg = () => {
+        data.weather[0].main == 'Clouds' ? clouds.classList.add('active') : clouds.classList.remove('active'); 
+    }
+    changeBg();
     const response2 = await fetch('https://api.api-ninjas.com/v1/worldtime?city=' + data.name, {
         headers: {
             'X-Api-Key': 'tHQpETr7UX243zBP+O58CA==gIctvZwydPGeErQI'
@@ -24,8 +31,9 @@ async function checkWeather(city){
     let data2 = await response2.json();
     console.log(data2);
     document.getElementById('time').innerHTML = data2.hour + ':' + data2.minute;
-
+    document.querySelector('.error').style.display = 'none'
 }
+};
 searchBtn.addEventListener('click', () =>{
-    checkWeather(input.value)
-});
+    checkWeather(input.value);
+})
